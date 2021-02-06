d9488a5eb6d93cdcfa2dfd1a7f96673e00409215
Khubaib <khubaib.dev@gmail.com>
2021-02-06 16:23:02 +0500

    Add theme

---
+++ src/app/styles/palette.ts
@@ -0,0 +1,33 @@
+export const BaseColorPalette = {
+  brand: {
+    main: "#00D1C7",
+  },
+  teal: {
+    main: "#05A39B",
+    secondary: "#76C3C0",
+    dark: "#007C76",
+    light: "#C4EFEE",
+  },
+  neutral: {
+    main: "#6B7E7D",
+    secondary: "#CDCBCB",
+    dark: "#323232",
+    light: "#F3F3F3",
+  },
+  red: {
+    main: "#DA3836",
+  },
+  green: {
+    main: "#27AE60",
+  },
+  yellow: {
+    main: "#F2BD38",
+  },
+  blue: {
+    main: "#56CCF2",
+  },
+  gray: {
+    main: "#828282",
+  },
+  white: "#FFFFFF",
+};
--- src/app/styles/theme.ts
+++ src/app/styles/theme.ts
@@ -1,5 +1,89 @@
+import { pxToRem } from '../../common/utils/styles';
 import { createMuiTheme, Theme } from '@material-ui/core/styles';
 
-const theme = createMuiTheme({});
+import { BaseColorPalette } from './palette';
+
+const theme: Theme = createMuiTheme({
+  palette: {
+    ...BaseColorPalette,
+    primary: {
+      main: BaseColorPalette.brand.main,
+    },
+    error: {
+      main: BaseColorPalette.red.main,
+    },
+  },
+  typography: {
+    fontFamily: 'Inter',
+    allVariants: {
+      color: BaseColorPalette.neutral.dark,
+      lineHeight: pxToRem(20),
+      fontSize: pxToRem(14),
+    },
+    button: {
+      height: pxToRem(36),
+      fontWeight: 600,
+      textTransform: 'none',
+    },
+    h1: {
+      fontSize: pxToRem(20),
+      fontWeight: 500,
+    },
+    h2: {
+      fontSize: pxToRem(16),
+      fontWeight: 500,
+    },
+  },
+  props: {
+    MuiButton: {
+      disableElevation: true,
+    },
+  },
+  overrides: {
+    MuiCssBaseline: {
+      '@global': {
+        a: {
+          color: 'inherit',
+          textDecoration: 'none',
+        },
+      },
+    },
+    MuiButton: {
+      root: {
+        '&$disabled': {
+          backgroundColor: `${BaseColorPalette.neutral.light} !important`,
+          color: BaseColorPalette.neutral.secondary,
+        },
+      },
+      containedPrimary: {
+        backgroundColor: BaseColorPalette.teal.main,
+        color: BaseColorPalette.white,
+        '&:hover': {
+          backgroundColor: BaseColorPalette.teal.dark,
+          color: BaseColorPalette.white,
+        },
+      },
+      containedSecondary: {
+        backgroundColor: BaseColorPalette.teal.light,
+        color: BaseColorPalette.teal.dark,
+        '&:hover': {
+          backgroundColor: BaseColorPalette.teal.secondary,
+          color: BaseColorPalette.teal.dark,
+        },
+      },
+      contained: {
+        backgroundColor: BaseColorPalette.neutral.light,
+        color: BaseColorPalette.neutral.dark,
+        '&:hover': {
+          backgroundColor: BaseColorPalette.neutral.light,
+          color: BaseColorPalette.teal.main,
+        },
+      },
+      startIcon: {
+        marginRight: pxToRem(6),
+      },
+    },
+  },
+});
 
 export default theme;
